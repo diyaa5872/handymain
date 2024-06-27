1248bec9b91f1f29227710997b3da7454005aa70
diyaa5872 <dhankhardiya.8@gmail.com>
2024-06-27 17:14:00 +0530

    difhiewfh

--- Backend/src/routes/booking.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { combinedHandler } from "../controllers/booking.controller.js"
-
-const router = Router()
-router.route('/bookandrequest').post(
-    combinedHandler
-)
-
-export default router;
