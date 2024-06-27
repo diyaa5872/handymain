445dd42efb67d6c92ad44347265ea06b1d6a77c3
diyaa5872 <dhankhardiya.8@gmail.com>
2024-06-27 14:50:42 +0530

    Fefew

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+      external: ['react-router-dom','axios'],
     },
   },
 })
